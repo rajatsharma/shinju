27efd37dfc48c445b8be4b0d68c77a48edc71734
Rajat Sharma <rajatsharma0720@gmail.com>
2018-07-05 22:45:46 +0530

    Added Listerners and Subscribers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 function Shinju (defaultTree = {}) {
   let tree = defaultTree
+  let listeners = []
+
+  function subscribe(fn){
+    listeners.push(fn)
+    return () => {
+      listeners = listeners.filter(l => l !== listener)
+    }
+  }
 
   function add (item, itemLens) {
+    listeners.map(x => x())
     tree = { ...tree, [itemLens]: item }
   }
 
